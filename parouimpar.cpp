a074b5d82aadaa0ca5a5ae1dca89ba2375c05df6
sfarinas <sfarinas@gmail.com>
2022-05-21 07:48:27 -0300

    first commit

---
+++ parouimpar.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+main() {
+    int menor, maior, aux;
+    int par = 0;
+
+    setlocale (LC_ALL,"portuguese");
+	
+	printf ("============================\n");
+	printf ("        Par ou Ímpar\n");
+	printf ("============================\n");
+    printf("Informe o número Inicial: \n");
+    scanf("%d", &menor);
+    printf("Informe o número Final: \n");
+    scanf("%d", &maior);
+
+    if (maior < menor)  {
+        printf ("\nERRO: Número Final não pode ser menor que o número Inicial\n");
+     }
+
+    for (aux = menor; aux <= maior ; aux++)  {
+        if (aux % 2 == 0) { 
+            printf ("O número %i é par \n",aux);
+            par++;
+        }
+        else {
+            printf("O número %i é impar \n",aux);
+        }
+    }
+    printf("Quantidade de números Pares: %d\n", par);
+}
